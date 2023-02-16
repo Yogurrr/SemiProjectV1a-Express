250f2365d674285d9b9b5ec625ef934406939b82
LeeSeungYeon <masakisqq@naver.com>
2023-02-16 10:32:14 +0900

    저장소 초기화

---
+++ app.js
@@ -0,0 +1,40 @@
+// express 모듈과 기타 미들웨어 모듈 사용 선언
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+// 라우팅 모듈 설정
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/user')
+const aboutRouter = require('./routes/about')
+// const {static} = require("express");
+
+// express 객체 생성 및 포트 변수 선언
+const app = express();
+// 포트가 정해져있지 않으면 3000번을 포트로 쓰겠다는 것
+const port = process.env.PORT || 3000;
+
+// 라우팅 없이 바로 호출 가능하도록 static 폴더 설정
+app.use(express.static(path.join(__dirname, 'static')));
+
+// 라우팅 모듈 등록 - 클라이언트 요청 처리 핵심 파트
+// 정상 처리일 때
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use('/about', aboutRouter);
+
+// 404, 500 응답코드에 대한 라우팅 처리 정의
+// 비정상 처리일 때(오류 발생)
+app.use((req, res) => {
+    res.status(404);
+    res.send('404 - 페이지가 없습니다!');
+});
+app.use((err, req, res, next) => {
+    res.status(500);
+    res.send('500 - 서버 내부 오류가 발생했습니다!')
+});
+
+// 위에서 설정한 사항들을 토대로 express 서버 실행
+app.listen(port, () => {
+    console.log('SemiProjectV1 서버 실행 중...');
+});
